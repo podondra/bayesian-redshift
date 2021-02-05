75e837f12ff56b41d8fc9f0f31c0a4c4e487e1c1
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2021-02-05 08:35:31 +0100

    Implement TensorBoard logging

--- Neural.jl
+++ Neural.jl
@@ -6,6 +6,8 @@ using Flux
 using Flux.Data: DataLoader
 using Flux.Losses: mse
 using HDF5
+using Logging
+using TensorBoardLogger
 
 export train_wrapper!, nn
 
@@ -33,9 +35,8 @@ function predict(model, X)
 end
 
 function train_with_early_stopping!(
-        model, X_train::Matrix{Float32}, y_train::Matrix{Float32},
-        X_validation::Matrix{Float32}, y_validation::Matrix{Float32};
-        batchsize::Int64, patience::Int64, device, file_model)
+        model, X_train, y_train, X_validation, y_validation;
+        batchsize, patience, device, file_model)
     model = model |> device
     X_train, y_train = X_train |> device, y_train |> device
     X_validation, y_validation = X_validation |> device, y_validation |> device
@@ -56,6 +57,7 @@ function train_with_early_stopping!(
         Flux.train!(loss, Θ, loader_train, optimizer)
         epoch += 1
 
+        loss_train = mse(predict(model, X_train), y_train)
         loss_validation = mse(predict(model, X_validation), y_validation)
         if loss_validation < loss_validation_star
             i = 0
@@ -64,13 +66,17 @@ function train_with_early_stopping!(
         else
             i += 1
         end
+        @info "loss" validation=loss_validation train=loss_train
     end
 end
 
-function train_wrapper!(model, file_model)
+function train_wrapper!(model, name_model)
+    logger = TBLogger("runs/" * name_model, tb_overwrite)
     X_train, y_train, X_validation, y_validation = get_data()
-    train_with_early_stopping!(
-        model, get_data()...,
-        batchsize=128, patience=16, device=gpu, file_model=file_model)
+    with_logger(logger) do
+        train_with_early_stopping!(
+            model, get_data()..., batchsize=128, patience=16, device=gpu,
+            file_model=name_model * ".bson")
+    end
 end
 end # module
