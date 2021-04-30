5005674d51a1eb8577604af2a348c43d3fc8e9e0
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2021-04-30 12:04:59 +0200

    Modify evaluation

--- evaluation.jl
+++ evaluation.jl
@@ -5,7 +5,7 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 364b618e-330c-4ae1-b70e-a8267028fee1
-using BSON, FITSIO, Flux, Flux.Data, HDF5, Printf, Statistics, StatsPlots
+using BSON, CUDA, FITSIO, Flux, Flux.Data, HDF5, Printf, Statistics, StatsPlots
 
 # ╔═╡ 6dc764b2-66e7-11eb-0833-9dc54a18f920
 begin
@@ -30,6 +30,9 @@ begin
 	size(X_train), size(X_validation)
 end
 
+# ╔═╡ 9bc66b89-d566-4335-a257-e228ff9d7e3e
+CUDA.versioninfo()
+
 # ╔═╡ 59c68c29-51c2-4cdd-9e74-567bc5a2cded
 md"## Pipeline Baseline"
 
@@ -43,35 +46,35 @@ end
 Evaluation.rmse(y_validation, y_pipe), Evaluation.catastrophic_redshift_ratio(y_validation, y_pipe)
 
 # ╔═╡ 646c8844-d25a-4453-a4d9-e6f6279c183b
-md"## Classical Model"
+md"## Regression Model"
 
 # ╔═╡ edd6e898-6797-11eb-2cee-791764fb425a
 begin
 	Core.eval(Main, :(import Flux, NNlib))
-	model = gpu(BSON.load("models/model.bson")[:model])
-	ŷ_train = cpu(Neural.predict(model, X_train))
-	ŷ_validation = cpu(Neural.predict(model, X_validation))
-	model
+	model_reg = gpu(BSON.load("models/regression_model.bson")[:model])
+	ŷ_train_reg = cpu(Neural.regress(model_reg, X_train))
+	ŷ_validation_reg = cpu(Neural.regress(model_reg, X_validation))
+	model_reg
 end
 
 # ╔═╡ d554d1f7-93d5-445a-8a74-ef9035bb2190
-Evaluation.rmse(y_train, ŷ_train), Evaluation.catastrophic_redshift_ratio(y_train, ŷ_train)
+Evaluation.rmse(y_train, ŷ_train_reg),
+Evaluation.catastrophic_redshift_ratio(y_train, ŷ_train_reg)
 
 # ╔═╡ 7b0e34c6-67ba-11eb-2500-378603362df8
-Evaluation.rmse(y_validation, ŷ_validation), Evaluation.catastrophic_redshift_ratio(y_validation, ŷ_validation)
+Evaluation.rmse(y_validation, ŷ_validation_reg), Evaluation.catastrophic_redshift_ratio(y_validation, ŷ_validation_reg)
 
 # ╔═╡ e6d5d6fa-6aab-11eb-0434-19c6a5a97099
 begin
 	density(y_validation, label="Visual", xlabel="z", ylabel="Density")
-	density!(ŷ_validation, label="Model")
+	density!(ŷ_validation_reg, label="Model")
 end
 
 # ╔═╡ 5f25ed6f-ef8d-49c0-993d-e3d5c445fd99
 begin
-	scatter_model = scatter(y_validation, ŷ_validation, legend=:none)
+	scatter_model_reg = scatter(y_validation, ŷ_validation_reg, legend=:none)
 	scatter_pipe = scatter(y_validation, y_pipe, legend=:none)
-	scatters = @layout [a b]
-	plot(scatter_model, scatter_pipe, layout=scatters)
+	plot(scatter_model_reg, scatter_pipe, layout=@layout [a b])
 end
 
 # ╔═╡ c05e8b4a-32d7-49b2-8a38-b7f040e1921c
@@ -79,7 +82,9 @@ begin
 	threshold_range = 1000:10:5000
 	plot(
 		threshold_range,
-		[Evaluation.catastrophic_redshift_ratio(y_validation, ŷ_validation, threshold=t) for t in threshold_range],
+		[Evaluation.catastrophic_redshift_ratio(
+				y_validation, ŷ_validation_reg, threshold=t)
+			for t in threshold_range],
 		label="Model", xlabel="Δv", ylabel="Cat. z")
 	plot!(
 		threshold_range,
@@ -89,7 +94,7 @@ end
 
 # ╔═╡ 1d52576a-6abb-11eb-2dd8-c388b2365ddd
 begin	
-	Δv = Evaluation.compute_delta_v(y_validation, ŷ_validation)
+	Δv_reg = Evaluation.compute_delta_v(y_validation, ŷ_validation_reg)
 	# random
 	i = rand(1:size(id_validation, 2))
 	# cat. z
@@ -98,8 +103,8 @@ begin
 	#i = sortperm(abs.(y_validation - ŷ_validation))[end]
 
 	z = y_validation[i]
-	ẑ = ŷ_validation[i]
-	title = @sprintf "z = %.3f; ẑ = %.3f; Δv = %.3f" z ẑ Δv[i]
+	ẑ = ŷ_validation_reg[i]
+	title = @sprintf "z = %.3f; ẑ = %.3f; Δv = %.3f" z ẑ Δv_reg[i]
 	Utils.plot_spectrum(X_validation[:, i], title=title, legend=:none)
 	Utils.plot_spectral_lines!(z)
 	prepared_spectrum = Utils.plot_spectral_lines!(ẑ, color=:red, location=:bottom)
@@ -107,6 +112,37 @@ begin
 	original_spectrum = plot(10 .^ loglam, flux, legend=:none)
 	l = @layout [a; b]
 	plot(prepared_spectrum, original_spectrum, layout=l)
+end
+
+# ╔═╡ c9c00f77-4f40-445c-b348-70754dfce19c
+md"## Classification Model"
+
+# ╔═╡ cd79f25b-36e0-463a-80b6-ebc622aa75d2
+begin
+	Core.eval(Main, :(import Flux, NNlib))
+	model_clf = gpu(BSON.load("models/classification_model.bson")[:model])
+	ŷ_train_clf = cpu(Neural.classify(model_clf, X_train))
+	ŷ_validation_clf = cpu(Neural.classify(model_clf, X_validation))
+	model_clf
+end
+
+# ╔═╡ e1d4a0f3-e1cb-4576-ae6a-97e735438236
+Evaluation.rmse(y_train, ŷ_train_clf),
+Evaluation.catastrophic_redshift_ratio(y_train, ŷ_train_clf)
+
+# ╔═╡ f8ec4620-fc02-4dca-b005-0961de8ed1af
+Evaluation.rmse(y_validation, ŷ_validation_clf), Evaluation.catastrophic_redshift_ratio(y_validation, ŷ_validation_clf)
+
+# ╔═╡ e1ada0e9-c547-4642-92e6-71a8ef1ce5ad
+begin
+	density(y_validation, label="Visual", xlabel="z", ylabel="Density")
+	density!(ŷ_validation_clf, label="Model")
+end
+
+# ╔═╡ 8d64cf38-8af9-4b35-89b2-daa60fdab019
+begin
+	scatter_model_clf = scatter(y_validation, ŷ_validation_clf, legend=:none)
+	plot(scatter_model_reg, scatter_pipe, layout=@layout [a b])
 end
 
 # ╔═╡ fe9a448e-fc02-4d70-b12b-954cdf472849
@@ -120,7 +156,7 @@ md"## Bayesian Model
 # ╔═╡ 92607376-6ac1-11eb-3620-1ff033ef6890
 function point_estimate(model, X; n_samples=64)
 	trainmode!(model)
-	outputs = reduce(hcat, [Neural.predict(model, X) for i in 1:n_samples])
+	outputs = reduce(hcat, [Neural.regress(model, X) for i in 1:n_samples])
 	ŷ_mean = dropdims(mean(outputs, dims=2), dims=2)
 	ŷ_std = dropdims(std(outputs, mean=ŷ_mean, dims=2), dims=2)
 	return ŷ_mean, ŷ_std
@@ -134,6 +170,13 @@ begin
 	ŷ_validation_bayes, σ_validation = cpu(point_estimate(model_bayes, X_validation))
 	model_bayes
 end
+
+# ╔═╡ 895d5f6b-88e6-4fa9-a9d9-0516e27fe30c
+Evaluation.rmse(y_train, ŷ_train_bayes),
+Evaluation.catastrophic_redshift_ratio(y_train, ŷ_train_bayes)
+
+# ╔═╡ 4d920462-4f1f-47a6-b0b1-dd3a656251c9
+Evaluation.rmse(y_validation, ŷ_validation_bayes), Evaluation.catastrophic_redshift_ratio(y_validation, ŷ_validation_bayes)
 
 # ╔═╡ a8c710e6-6ac8-11eb-3f00-5d2b72864754
 begin
@@ -174,6 +217,7 @@ end
 # ╠═364b618e-330c-4ae1-b70e-a8267028fee1
 # ╠═6dc764b2-66e7-11eb-0833-9dc54a18f920
 # ╠═8643602a-66e9-11eb-3700-374047551428
+# ╠═9bc66b89-d566-4335-a257-e228ff9d7e3e
 # ╟─59c68c29-51c2-4cdd-9e74-567bc5a2cded
 # ╠═06f9cf4e-e341-410a-83a4-4d6a5ea5576a
 # ╠═61d4b6de-5efa-400b-b4b0-cbccab2d9f6b
@@ -185,9 +229,17 @@ end
 # ╠═5f25ed6f-ef8d-49c0-993d-e3d5c445fd99
 # ╠═c05e8b4a-32d7-49b2-8a38-b7f040e1921c
 # ╠═1d52576a-6abb-11eb-2dd8-c388b2365ddd
+# ╟─c9c00f77-4f40-445c-b348-70754dfce19c
+# ╠═cd79f25b-36e0-463a-80b6-ebc622aa75d2
+# ╠═e1d4a0f3-e1cb-4576-ae6a-97e735438236
+# ╠═f8ec4620-fc02-4dca-b005-0961de8ed1af
+# ╠═e1ada0e9-c547-4642-92e6-71a8ef1ce5ad
+# ╠═8d64cf38-8af9-4b35-89b2-daa60fdab019
 # ╟─fe9a448e-fc02-4d70-b12b-954cdf472849
 # ╠═92607376-6ac1-11eb-3620-1ff033ef6890
 # ╠═08c367cc-3c81-450c-97c5-96c6b80c7a0f
+# ╠═895d5f6b-88e6-4fa9-a9d9-0516e27fe30c
+# ╠═4d920462-4f1f-47a6-b0b1-dd3a656251c9
 # ╠═a8c710e6-6ac8-11eb-3f00-5d2b72864754
 # ╠═e396f046-71ce-11eb-1564-03d296917d94
 # ╠═92142f12-6acd-11eb-32c9-9f33783e75f4
