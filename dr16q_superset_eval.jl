15c25b72222a471ce563af898667b1c198bdee36
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2021-06-21 13:53:17 +0200

    Add interactive plotting

--- dr16q_superset_eval.jl
+++ dr16q_superset_eval.jl
@@ -1,32 +1,36 @@
 ### A Pluto.jl notebook ###
-# v0.14.7
+# v0.14.8
 
 using Markdown
 using InteractiveUtils
 
 # ╔═╡ bdb521fa-acd6-11eb-0d41-2d68a7abecb2
-begin
-	using BSON, CUDA, DataFrames, Flux, HDF5, Printf, Statistics, StatsPlots
-	include("Evaluation.jl"); using .Evaluation
-	include("Neural.jl"); using .Neural
-	include("Utils.jl"); using .Utils
-	Core.eval(Main, :(import Flux, NNlib))
-end
+using BSON, DataFrames, Flux, HDF5, Printf, Statistics, StatsBase, StatsPlots
+
+# ╔═╡ 5c29de0c-4c3f-44ea-aea3-dbc0449a4a22
+include("Evaluation.jl"); import .Evaluation
+
+# ╔═╡ 8f22d6ac-b9f5-4437-97e0-cb5fb6006644
+include("Neural.jl"); import .Neural
+
+# ╔═╡ 62fd8899-7fe0-4d54-9326-79008c60140b
+include("Utils.jl"); import .Utils
+
+# ╔═╡ 8c971f02-0dab-41d0-928b-7937052f7542
+plotly()
+
+# ╔═╡ a96802e3-1df4-4bf7-b0d1-4e2ad106f093
+Core.eval(Main, :(import Flux, NNlib))
 
 # ╔═╡ a13527ac-4670-4a2f-a390-17700d707705
 begin
 	dr16q_file = h5open("data/dr16q_superset.hdf5", "r")
-	X = read(dr16q_file, "flux")
-
-	model = BSON.load("models/classification_1e-4.bson")[:model] |> gpu
-	ẑ = Neural.classify(model, X |> gpu) |> cpu
-
+	X = read(dr16q_file, "X")
 	id = read(dr16q_file, "id")
 	df = DataFrame(
 		plate=id[1, :],
 		mjd=id[2, :],
 		fiberid=id[3, :],
-		ẑ=ẑ,
 		source=read(dr16q_file, "source_z"),
 		z = read(dr16q_file, "z"),
 		z_10k=read(dr16q_file, "z_10k"),
@@ -39,40 +43,49 @@ begin
 end
 
 # ╔═╡ dd19c4bb-92fd-42f6-b77b-bb0190ac793e
-df_10k = df[df[:z_10k] .> -1, :]
+begin
+	idx_10k = df[!, :z_10k] .> -1
+	df_10k = df[idx_10k, :]
+end
 
-# ╔═╡ 2b9b1d9e-7d19-47a2-b5ac-853f00a489f7
-@df df_10k histogram(:z_10k)
+# ╔═╡ 1ff62a81-d0ad-44be-8f82-65487082fb25
+X_10k = X[:, idx_10k]
+
+# ╔═╡ ed949c9a-3222-4209-b8b2-1eb7383bcf93
+begin
+	model = BSON.load("models/mc_dropout_model.bson")[:model] |> gpu
+	ẑ_10k = Neural.mc_dropout(model, X_10k |> gpu, t=20) |> cpu
+end
 
 # ╔═╡ dc462c4d-ec37-4459-87e6-87428f2229da
-Evaluation.rmse(df_10k[:z_10k], df_10k[:z_pca]),
-Evaluation.median_Δv(df_10k[:z_10k], df_10k[:z_pca]),
-Evaluation.cat_z_ratio(df_10k[:z_10k], df_10k[:z_pca])
+Evaluation.rmse(df_10k[!, :z_10k], df_10k[!, :z_pca]),
+Evaluation.median_Δv(df_10k[!, :z_10k], df_10k[!, :z_pca]),
+Evaluation.cat_z_ratio(df_10k[!, :z_10k], df_10k[!, :z_pca])
 
 # ╔═╡ cf7c4ece-6bf8-4e53-85c1-1acdb2d37be1
-Evaluation.rmse(df_10k[:z_10k], df_10k[:z_pipe]),
-Evaluation.median_Δv(df_10k[:z_10k], df_10k[:z_pipe]),
-Evaluation.cat_z_ratio(df_10k[:z_10k], df_10k[:z_pipe])
+Evaluation.rmse(df_10k[!, :z_10k], df_10k[!, :z_pipe]),
+Evaluation.median_Δv(df_10k[!, :z_10k], df_10k[!, :z_pipe]),
+Evaluation.cat_z_ratio(df_10k[!, :z_10k], df_10k[!, :z_pipe])
 
 # ╔═╡ b69e11ff-1249-43fb-a423-61efa0945030
-Evaluation.rmse(df_10k[:z_10k], df_10k[:z_qn]),
-Evaluation.median_Δv(df_10k[:z_10k], df_10k[:z_qn]),
-Evaluation.cat_z_ratio(df_10k[:z_10k], df_10k[:z_qn])
+Evaluation.rmse(df_10k[!, :z_10k], df_10k[!, :z_qn]),
+Evaluation.median_Δv(df_10k[!, :z_10k], df_10k[!, :z_qn]),
+Evaluation.cat_z_ratio(df_10k[!, :z_10k], df_10k[!, :z_qn])
 
 # ╔═╡ 0211a7d5-f744-4331-a55c-6860716c2109
-Evaluation.rmse(df_10k[:z_10k], df_10k[:ẑ]),
-Evaluation.median_Δv(df_10k[:z_10k], df_10k[:ẑ]),
-Evaluation.cat_z_ratio(df_10k[:z_10k], df_10k[:ẑ])
+Evaluation.rmse(df_10k[!, :z_10k], ẑ_10k),
+Evaluation.median_Δv(df_10k[!, :z_10k], ẑ_10k),
+Evaluation.cat_z_ratio(df_10k[!, :z_10k], ẑ_10k)
 
 # ╔═╡ d1770126-cb47-47ae-844a-268210927dfb
 begin
 	@df df_10k density(:z_10k, label="Reference", xlabel="z", ylabel="Density")
-	@df df_10k density!(:ẑ, label="Clf Model")
+	@df df_10k density!(ẑ_10k, label="MC Dropout")
 end
 
 # ╔═╡ 61f99ee5-46c0-403a-8fca-ab33f491fad8
 begin
-	Δv = Evaluation.compute_Δv(df_10k[:z], df_10k[:ẑ])
+	Δv = Evaluation.compute_Δv(df_10k[!, :z], ẑ_10k)
 	# random
 	i = rand(1:size(df_10k, 1))
 	# cat. z
@@ -82,15 +95,10 @@ begin
 
 	title = @sprintf(
 		"z = %.3f; ẑ = %.2f; Δv = %.3f",
-		df_10k[i, :z], df_10k[i, :ẑ], Δv[i])
-	Utils.plot_spectrum(X[:, df[:z_10k] .> -1][:, i], title=title, legend=:none)
+		df_10k[i, :z], ẑ_10k[i], Δv[i])
+	Utils.plot_spectrum(X[:, df[!, :z_10k] .> -1][:, i], title=title, legend=:none)
 	Utils.plot_spectral_lines!(df_10k[i, :z])
-	prep_spec = Utils.plot_spectral_lines!(
-		df_10k[i, :ẑ], color=:red, location=:bottom)
-	loglam, flux = Utils.get_spectrum(
-		"DR16Q_Superset_v3", df_10k[i, :plate], df_10k[i, :mjd], df_10k[i, :fiberid])
-	orig_spec = plot(10 .^ loglam, flux, legend=:none)
-	plot(prep_spec, orig_spec, layout=@layout [a; b])
+	Utils.plot_spectral_lines!(ẑ_10k[i], color=:red, location=:bottom)
 end
 
 # ╔═╡ eb11930b-8f4f-4301-a462-a41fa54d980f
@@ -98,19 +106,19 @@ md"## Uncertainties"
 
 # ╔═╡ fe97db35-4c07-4c3c-9429-98665884d921
 begin
-	bayes = BSON.load("models/classification_1e-4.bson")[:model] |> gpu
+	bayes = BSON.load("models/mc_dropout_model.bson")[:model] |> gpu
 	trainmode!(bayes)
 end
 
 # ╔═╡ bb20eec3-c007-41ce-9881-5d956dd76c80
 begin
-	n = size(X, 2)
-	batch_size = 128
+	n = size(X_10k, 2)
+	batch_size = 20
 	ẑs = zeros(batch_size, n)
 	entropies = zeros(n)
 	mutual_infos = zeros(n)
 	for idx in 1:n
-		out = softmax(bayes(X[:, [idx for i in 1:batch_size]] |> gpu) |> cpu)
+		out = softmax(bayes(X_10k[:, [idx for i in 1:batch_size]] |> gpu)) |> cpu
 		ẑs[:, idx] = Flux.onecold(out, 0.00f0:0.01f0:6.44f0)
 		foo = mean(out, dims=2)
 		entropies[idx] = -sum(foo .* log.(foo .+ eps(Float32)))
@@ -120,11 +128,82 @@ begin
 	ẑs, entropies, mutual_infos
 end
 
+# ╔═╡ 07d44911-4a18-4958-b790-ac09115af345
+sortperm(mutual_infos)
+
+# ╔═╡ 59d11a76-9ef6-4881-8d14-89ae065f3898
+begin
+	idx = 2530
+	countmap(ẑs[:, idx])
+end
+
+# ╔═╡ 77108e12-ad9e-418c-bd02-194cb5a891c4
+begin
+	StatsPlots.histogram(entropies, label="Entropy")
+	StatsPlots.histogram!(mutual_infos, label="Mutual Information")
+end
+
+# ╔═╡ 8928bf18-dfde-4f8b-a9dc-a70c14d52d84
+begin
+	ẑ_bayes = zeros(n)
+	for i in 1:n
+		ẑ_bayes[i] = findmax(countmap(ẑs[:, i]))[2]
+	end
+	ẑ_bayes
+end
+
+# ╔═╡ b6a4ead6-aa18-4aa0-be28-50e698eae60c
+begin
+	ts = 0.001:0.001:maximum(mutual_infos)
+	y_10k = df_10k[!, :z_10k]
+	Plots.plot(
+		Plots.plot(
+			ts,
+			[sum(mutual_infos .< t) / length(mutual_infos) for t in ts],
+			label="Completeness", xlabel="Threshold"),
+		Plots.plot(
+			ts,
+			[Evaluation.cat_z_ratio(
+					y_10k[mutual_infos .< t], ẑ_bayes[mutual_infos .< t])
+				for t in ts],
+			label="Catastrophic z Ratio", xlabel="Threshold"),
+		layout=@layout [a; b])
+end
+
+# ╔═╡ ee0846f6-c87c-4c5f-9254-99ee70c010f8
+begin
+	# absolute error
+	Δv_bayes = Evaluation.compute_Δv(y_10k, ẑ_bayes)
+	#j = sortperm(mutual_infos)[end]
+	#j = rand((1:n)[Δv_bayes .> 3000])
+	j = rand((1:n)[mutual_infos .> 0.6])
+	#j = rand((1:n)[(mutual_infos .< 0.1) .& (Δv_bayes .> 3000)])
+	#j = rand((1:n)[(y_10k .< 0.1) .& (ẑ_bayes .> 0.5)])
+	
+	countmap(ẑs[:, j])
+end
+
+# ╔═╡ 9674fd8d-7d95-419e-95ce-8101b232e1dd
+begin
+	title_bayes = @sprintf(
+		"z = %.3f; ẑ = %.2f; Δv = %.2f; MI = %.3f; %s",
+		y_10k[j], ẑ_bayes[j], Δv_bayes[j], mutual_infos[j], df_10k[j, :source])
+	Utils.plot_spectrum(X_10k[:, j], title=title_bayes, legend=:none)
+	Utils.plot_spectral_lines!(y_10k[j])
+	Utils.plot_spectral_lines!(ẑ_bayes[j], color=:red, location=:bottom)
+end
+
 # ╔═╡ Cell order:
 # ╠═bdb521fa-acd6-11eb-0d41-2d68a7abecb2
+# ╠═5c29de0c-4c3f-44ea-aea3-dbc0449a4a22
+# ╠═8f22d6ac-b9f5-4437-97e0-cb5fb6006644
+# ╠═62fd8899-7fe0-4d54-9326-79008c60140b
+# ╠═8c971f02-0dab-41d0-928b-7937052f7542
+# ╠═a96802e3-1df4-4bf7-b0d1-4e2ad106f093
 # ╠═a13527ac-4670-4a2f-a390-17700d707705
 # ╠═dd19c4bb-92fd-42f6-b77b-bb0190ac793e
-# ╠═2b9b1d9e-7d19-47a2-b5ac-853f00a489f7
+# ╠═1ff62a81-d0ad-44be-8f82-65487082fb25
+# ╠═ed949c9a-3222-4209-b8b2-1eb7383bcf93
 # ╠═dc462c4d-ec37-4459-87e6-87428f2229da
 # ╠═cf7c4ece-6bf8-4e53-85c1-1acdb2d37be1
 # ╠═b69e11ff-1249-43fb-a423-61efa0945030
@@ -134,3 +213,10 @@ end
 # ╟─eb11930b-8f4f-4301-a462-a41fa54d980f
 # ╠═fe97db35-4c07-4c3c-9429-98665884d921
 # ╠═bb20eec3-c007-41ce-9881-5d956dd76c80
+# ╠═07d44911-4a18-4958-b790-ac09115af345
+# ╠═59d11a76-9ef6-4881-8d14-89ae065f3898
+# ╠═77108e12-ad9e-418c-bd02-194cb5a891c4
+# ╠═8928bf18-dfde-4f8b-a9dc-a70c14d52d84
+# ╠═b6a4ead6-aa18-4aa0-be28-50e698eae60c
+# ╠═ee0846f6-c87c-4c5f-9254-99ee70c010f8
+# ╠═9674fd8d-7d95-419e-95ce-8101b232e1dd
