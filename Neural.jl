80ece0a0a013fc662d5fdea53375d05ed1cc27b8
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2021-06-24 15:47:59 +0200

    Refactor inference

--- Neural.jl
+++ Neural.jl
@@ -17,9 +17,12 @@ using TensorBoardLogger
 include("Evaluation.jl")
 using .Evaluation
 
-export dropout_model, regression_model, classification_model, classify,
-       regress, train_wrapper_regression!, train_wrapper_classification!,
-       train_wrapper_mc_dropout!
+export classification_model, regression_model,
+       classify, mc_dropout, regress,
+       train_wrapper_regression!, train_wrapper_classification!, train_wrapper_mc_dropout!
+
+const LABELS = 0.00f0:0.01f0:6.44f0
+const N_LABELS = length(LABELS)
 
 function classification_model()
     Chain(
@@ -42,39 +45,7 @@ function classification_model()
         Dropout(0.5),
         Dense(512, 512, relu),
         Dropout(0.5),
-        Dense(512, 645))
-end
-
-function dropout_model()
-    Chain(
-        Flux.unsqueeze(2),
-        Conv((3,), 1=>8, relu, pad=SamePad()),
-        Dropout(0.5),
-        MaxPool((2,)),
-        Conv((3,), 8=>16, relu, pad=SamePad()),
-        Dropout(0.5),
-        MaxPool((2,)),
-        Conv((3,), 16=>32, relu, pad=SamePad()),
-        Dropout(0.5),
-        Conv((3,), 32=>32, relu, pad=SamePad()),
-        Dropout(0.5),
-        MaxPool((2,)),
-        Conv((3,), 32=>64, relu, pad=SamePad()),
-        Dropout(0.5),
-        Conv((3,), 64=>64, relu, pad=SamePad()),
-        Dropout(0.5),
-        MaxPool((2,)),
-        Conv((3,), 64=>64, relu, pad=SamePad()),
-        Dropout(0.5),
-        Conv((3,), 64=>64, relu, pad=SamePad()),
-        Dropout(0.5),
-        MaxPool((2,)),
-        flatten,
-        Dense(7488, 512, relu),
-        Dropout(0.5),
-        Dense(512, 512, relu),
-        Dropout(0.5),
-        Dense(512, 645))
+        Dense(512, N_LABELS))
 end
 
 function regression_model()
@@ -108,6 +79,7 @@ function get_data()
     X_validation = read(datafile, "X_va")
     y_validation = read(datafile, "z_vi_va")
     close(datafile)
+
     return X_train, Flux.unsqueeze(y_train, 1), y_train, X_validation, y_validation
 end
 
@@ -120,44 +92,44 @@ function get_classification_data()
     close(datafile)
 
     EDGES = -0.005f0:0.01f0:6.445f0
-    LABELS = [@sprintf "%.2f" label for label in 0:0.01:6.44]
+    STR_LABELS = [@sprintf "%.2f" label for label in 0:0.01:6.44]
     # z smaller than 0 should be zero
     y_train[y_train .< 0] .= 0
-    y_train_categorical = cut(y_train, EDGES, labels=LABELS)
-    y_train_onehot = Flux.onehotbatch(y_train_categorical, LABELS)
+    y_train_categorical = cut(y_train, EDGES, labels=STR_LABELS)
+    y_train_onehot = Flux.onehotbatch(y_train_categorical, STR_LABELS)
 
     return X_train, y_train_onehot, y_train, X_validation, y_validation
 end
 
+function forward_pass(model, X)
+    reduce(hcat, cpu([model(x) for x in DataLoader(X, batchsize=2048)]))
+end
+
 function regress(model, X)
-    loader = DataLoader(X, batchsize=2048)
-    return dropdims(reduce(hcat, [model(x) for x in loader]), dims=1)
+    dropdims(forward_pass(model, X), dims=1)
 end
 
 function classify(model, X)
-    loader = DataLoader(X, batchsize=2048)
-    return reduce(vcat, [Flux.onecold(model(x), 0.00f0:0.01f0:6.44f0) for x in loader])
+    Flux.onecold(forward_pass(model, X), LABELS)
 end
 
-function mc_dropout(model, X; t)
+function mc_dropout(model, X; T=20)
     trainmode!(model)
-    loader = DataLoader(X, batchsize=2048)
-    probabilities = zeros(645, size(X, 2)) |> gpu
-    for i in 1:t
-        probabilities += reduce(hcat, [softmax(model(x)) for x in loader])
+    p = zeros(N_LABELS, size(X, 2))
+    for i in 1:T
+        p += softmax(forward_pass(model, X))
     end
-    return Flux.onecold(probabilities / t, 0.00f0:0.01f0:6.44f0)
+    return Flux.onecold(p / T, LABELS)
 end
 
 function train_with_early_stopping!(
         model, X_train, y_train_encoded, y_train, X_validation, y_validation;
-        loss, predict, batchsize, patience, weight_decay, device, file_model)
-    model = model |> device
-    X_train, y_train_encoded = X_train |> device, y_train_encoded |> device
-    X_validation = X_validation |> device
+        loss, predict, batchsize, patience, weight_decay, file_model)
+    model = gpu(model)
+    X_train, y_train_encoded = gpu(X_train), gpu(y_train_encoded)
+    X_validation = gpu(X_validation)
 
-    loader_train = DataLoader(
-        (X_train, y_train_encoded), batchsize=batchsize, shuffle=true)
+    loader_train = DataLoader((X_train, y_train_encoded), batchsize=batchsize, shuffle=true)
 
     optimizer = Optimiser(WeightDecay(weight_decay), ADAM())
     Θ = params(model)
@@ -171,8 +143,8 @@ function train_with_early_stopping!(
         Flux.train!(loss_function, Θ, loader_train, optimizer)
         epoch += 1
 
-        ŷ_validation = predict(model, X_validation) |> cpu
-        #ŷ_train = predict(model, X_train) |> cpu
+        ŷ_validation = predict(model, X_validation)
+        #ŷ_train = predict(model, X_train)
 
         rmse_validation = rmse(y_validation, ŷ_validation)
         #rmse_train = rmse(y_train, ŷ_train)
@@ -197,7 +169,7 @@ function train_wrapper_regression!(model, model_name; bs=256, wd=0)
         train_with_early_stopping!(
             model, get_data()...,
             loss=mse, predict=regress,
-            batchsize=bs, patience=32, weight_decay=wd, device=gpu,
+            batchsize=bs, patience=32, weight_decay=wd,
             file_model="models/" * model_name * ".bson")
     end
 end
@@ -208,7 +180,7 @@ function train_wrapper_classification!(model, model_name; bs=256, wd=0)
         train_with_early_stopping!(
             model, get_classification_data()...,
             loss=logitcrossentropy, predict=classify,
-            batchsize=bs, patience=32, weight_decay=wd, device=gpu,
+            batchsize=bs, patience=32, weight_decay=wd,
             file_model="models/" * model_name * ".bson")
     end
 end
@@ -218,8 +190,8 @@ function train_wrapper_mc_dropout!(model, model_name; bs=256, wd=1e-4)
     with_logger(logger) do
         train_with_early_stopping!(
             model, get_classification_data()...,
-            loss=logitcrossentropy, predict=(model, X) -> mc_dropout(model, X, t=20),
-            batchsize=bs, patience=32, weight_decay=wd, device=gpu,
+            loss=logitcrossentropy, predict=mc_dropout,
+            batchsize=bs, patience=32, weight_decay=wd,
             file_model="models/" * model_name * ".bson")
     end
 end
