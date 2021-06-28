aa387b4b58be18d13e6e11c93934e56fee31c894
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2021-06-28 11:31:22 +0200

    Add spectral lines

--- Utils.jl
+++ Utils.jl
@@ -12,14 +12,33 @@ N_FEATURES = 3752
 LAMBDA = 10 .^ range(LOGLAMMIN, LOGLAMMAX, length=N_FEATURES)
 
 # http://classic.sdss.org/dr6/algorithms/linestable.html
-LINES = Dict("O VI" => 1033.82,
-             "Lyα" => 1215.24,
-             "C IV" => 1549.48,
-             "C III" => 1908.734,
-             "Mg II" => 2799.117,
-             "O III" => 1665.85,
-             "Hα" => 6564.61,
-             "Hβ" => 4862.68)
+LINES = Dict(1033.82 => "O VI",
+             1215.24 => "Lyα",
+             1240.81 => "N V",
+             1399. => "Si IV + O IV",
+             1549.48 => "C IV",
+             1908.734 => "C III",
+             2326.0 => "C II",
+             2799.117 => "Mg II",
+             3727.092 => "O II",
+             3934.777 => "K",
+             3969.588 => "H",
+             4102.89 =>"HΔ",
+             4305.61 => "G",
+             4341.68 => "Hγ",
+             4862.68 => "Hβ",
+             4960.295 => "O III",
+             5008.240 => "O III",
+             5176.7 => "Mg",
+             5895.6 => "Na",
+             6549.86 => "N II",
+             6564.61 => "Hα",
+             6585.27 => "N II",
+             6718.29 => "S II",
+             6732.67 => "S II",
+             8500.36 => "Ca II",
+             8544.44 => "Ca II",
+             8664.52 => "Ca II")
 
 function get_filename(plate, mjd, fiberid)
     @sprintf("%04d/spec-%04d-%05d-%04d.fits", plate, plate, mjd, fiberid)
@@ -53,11 +72,11 @@ end
 
 function plot_spectral_lines!(z; color=:black, location=:top)
     scatter!(
-        [z2λ_obsv(z, λ_emit) for λ_emit in values(LINES)],
-        zeros(length(values(LINES))),
+        [z2λ_obsv(z, λ_emit) for λ_emit in keys(LINES)],
+        zeros(length(keys(LINES))),
         xlim=(LAMBDA[1], LAMBDA[end]),
         marker=(:vline, color), label=:none,
-        series_annotation=[text(line, 8, location) for line in keys(LINES)])
+        series_annotation=[text(line, 8, location) for line in values(LINES)])
 end
 
 function plot_spectrum(flux; kwargs...)
